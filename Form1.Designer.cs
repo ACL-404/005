23b67eb6bc5d47263e8171c2079add31774fe695
ACL-404 <zxcsd0919313688@gmail.com>
2025-03-24 15:30:33 +0800

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,283 @@
+﻿namespace _005
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BT1 = new System.Windows.Forms.Button();
+            this.BT2 = new System.Windows.Forms.Button();
+            this.BT3 = new System.Windows.Forms.Button();
+            this.BT4 = new System.Windows.Forms.Button();
+            this.BT5 = new System.Windows.Forms.Button();
+            this.BT6 = new System.Windows.Forms.Button();
+            this.BT7 = new System.Windows.Forms.Button();
+            this.BT8 = new System.Windows.Forms.Button();
+            this.BT9 = new System.Windows.Forms.Button();
+            this.BTPlus = new System.Windows.Forms.Button();
+            this.BTMinus = new System.Windows.Forms.Button();
+            this.BTadd = new System.Windows.Forms.Button();
+            this.BTDivide = new System.Windows.Forms.Button();
+            this.BTr = new System.Windows.Forms.Button();
+            this.BT0 = new System.Windows.Forms.Button();
+            this.BTp = new System.Windows.Forms.Button();
+            this.BTa = new System.Windows.Forms.Button();
+            this.TEXTB1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BT1
+            // 
+            this.BT1.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BT1.Location = new System.Drawing.Point(12, 191);
+            this.BT1.Name = "BT1";
+            this.BT1.Size = new System.Drawing.Size(83, 51);
+            this.BT1.TabIndex = 0;
+            this.BT1.Text = "1";
+            this.BT1.UseVisualStyleBackColor = true;
+            // 
+            // BT2
+            // 
+            this.BT2.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BT2.Location = new System.Drawing.Point(101, 191);
+            this.BT2.Name = "BT2";
+            this.BT2.Size = new System.Drawing.Size(83, 51);
+            this.BT2.TabIndex = 1;
+            this.BT2.Text = "2";
+            this.BT2.UseVisualStyleBackColor = true;
+            // 
+            // BT3
+            // 
+            this.BT3.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BT3.Location = new System.Drawing.Point(190, 191);
+            this.BT3.Name = "BT3";
+            this.BT3.Size = new System.Drawing.Size(83, 51);
+            this.BT3.TabIndex = 2;
+            this.BT3.Text = "3";
+            this.BT3.UseVisualStyleBackColor = true;
+            // 
+            // BT4
+            // 
+            this.BT4.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BT4.Location = new System.Drawing.Point(12, 134);
+            this.BT4.Name = "BT4";
+            this.BT4.Size = new System.Drawing.Size(83, 51);
+            this.BT4.TabIndex = 3;
+            this.BT4.Text = "4";
+            this.BT4.UseVisualStyleBackColor = true;
+            // 
+            // BT5
+            // 
+            this.BT5.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BT5.Location = new System.Drawing.Point(101, 134);
+            this.BT5.Name = "BT5";
+            this.BT5.Size = new System.Drawing.Size(83, 51);
+            this.BT5.TabIndex = 4;
+            this.BT5.Text = "5";
+            this.BT5.UseVisualStyleBackColor = true;
+            // 
+            // BT6
+            // 
+            this.BT6.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BT6.Location = new System.Drawing.Point(190, 134);
+            this.BT6.Name = "BT6";
+            this.BT6.Size = new System.Drawing.Size(83, 51);
+            this.BT6.TabIndex = 5;
+            this.BT6.Text = "6";
+            this.BT6.UseVisualStyleBackColor = true;
+            // 
+            // BT7
+            // 
+            this.BT7.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BT7.Location = new System.Drawing.Point(12, 77);
+            this.BT7.Name = "BT7";
+            this.BT7.Size = new System.Drawing.Size(83, 51);
+            this.BT7.TabIndex = 6;
+            this.BT7.Text = "7";
+            this.BT7.UseVisualStyleBackColor = true;
+            // 
+            // BT8
+            // 
+            this.BT8.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BT8.Location = new System.Drawing.Point(101, 77);
+            this.BT8.Name = "BT8";
+            this.BT8.Size = new System.Drawing.Size(83, 51);
+            this.BT8.TabIndex = 7;
+            this.BT8.Text = "8";
+            this.BT8.UseVisualStyleBackColor = true;
+            // 
+            // BT9
+            // 
+            this.BT9.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BT9.Location = new System.Drawing.Point(190, 77);
+            this.BT9.Name = "BT9";
+            this.BT9.Size = new System.Drawing.Size(83, 51);
+            this.BT9.TabIndex = 8;
+            this.BT9.Text = "9";
+            this.BT9.UseVisualStyleBackColor = true;
+            // 
+            // BTPlus
+            // 
+            this.BTPlus.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BTPlus.Location = new System.Drawing.Point(284, 191);
+            this.BTPlus.Name = "BTPlus";
+            this.BTPlus.Size = new System.Drawing.Size(83, 51);
+            this.BTPlus.TabIndex = 9;
+            this.BTPlus.Text = "*";
+            this.BTPlus.UseVisualStyleBackColor = true;
+            // 
+            // BTMinus
+            // 
+            this.BTMinus.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BTMinus.Location = new System.Drawing.Point(284, 134);
+            this.BTMinus.Name = "BTMinus";
+            this.BTMinus.Size = new System.Drawing.Size(83, 51);
+            this.BTMinus.TabIndex = 10;
+            this.BTMinus.Text = "-";
+            this.BTMinus.UseVisualStyleBackColor = true;
+            // 
+            // BTadd
+            // 
+            this.BTadd.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BTadd.Location = new System.Drawing.Point(284, 77);
+            this.BTadd.Name = "BTadd";
+            this.BTadd.Size = new System.Drawing.Size(83, 51);
+            this.BTadd.TabIndex = 11;
+            this.BTadd.Text = "+";
+            this.BTadd.UseVisualStyleBackColor = true;
+            // 
+            // BTDivide
+            // 
+            this.BTDivide.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BTDivide.Location = new System.Drawing.Point(284, 248);
+            this.BTDivide.Name = "BTDivide";
+            this.BTDivide.Size = new System.Drawing.Size(83, 51);
+            this.BTDivide.TabIndex = 12;
+            this.BTDivide.Text = "/";
+            this.BTDivide.UseVisualStyleBackColor = true;
+            // 
+            // BTr
+            // 
+            this.BTr.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BTr.Location = new System.Drawing.Point(284, 20);
+            this.BTr.Name = "BTr";
+            this.BTr.Size = new System.Drawing.Size(83, 51);
+            this.BTr.TabIndex = 13;
+            this.BTr.Text = "AC";
+            this.BTr.UseVisualStyleBackColor = true;
+            // 
+            // BT0
+            // 
+            this.BT0.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BT0.Location = new System.Drawing.Point(101, 248);
+            this.BT0.Name = "BT0";
+            this.BT0.Size = new System.Drawing.Size(83, 51);
+            this.BT0.TabIndex = 14;
+            this.BT0.Text = "0";
+            this.BT0.UseVisualStyleBackColor = true;
+            // 
+            // BTp
+            // 
+            this.BTp.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BTp.Location = new System.Drawing.Point(12, 248);
+            this.BTp.Name = "BTp";
+            this.BTp.Size = new System.Drawing.Size(83, 51);
+            this.BTp.TabIndex = 15;
+            this.BTp.Text = ".";
+            this.BTp.UseVisualStyleBackColor = true;
+            // 
+            // BTa
+            // 
+            this.BTa.Font = new System.Drawing.Font("新細明體", 18F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.BTa.Location = new System.Drawing.Point(190, 248);
+            this.BTa.Name = "BTa";
+            this.BTa.Size = new System.Drawing.Size(83, 51);
+            this.BTa.TabIndex = 16;
+            this.BTa.Text = "=";
+            this.BTa.UseVisualStyleBackColor = true;
+            // 
+            // TEXTB1
+            // 
+            this.TEXTB1.Font = new System.Drawing.Font("新細明體", 25.8F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.TEXTB1.Location = new System.Drawing.Point(12, 12);
+            this.TEXTB1.Name = "TEXTB1";
+            this.TEXTB1.Size = new System.Drawing.Size(266, 59);
+            this.TEXTB1.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Info;
+            this.ClientSize = new System.Drawing.Size(376, 319);
+            this.Controls.Add(this.TEXTB1);
+            this.Controls.Add(this.BTa);
+            this.Controls.Add(this.BTp);
+            this.Controls.Add(this.BT0);
+            this.Controls.Add(this.BTr);
+            this.Controls.Add(this.BTDivide);
+            this.Controls.Add(this.BTadd);
+            this.Controls.Add(this.BTMinus);
+            this.Controls.Add(this.BTPlus);
+            this.Controls.Add(this.BT9);
+            this.Controls.Add(this.BT8);
+            this.Controls.Add(this.BT7);
+            this.Controls.Add(this.BT6);
+            this.Controls.Add(this.BT5);
+            this.Controls.Add(this.BT4);
+            this.Controls.Add(this.BT3);
+            this.Controls.Add(this.BT2);
+            this.Controls.Add(this.BT1);
+            this.Font = new System.Drawing.Font("新細明體", 9F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.Name = "Form1";
+            this.Text = "AC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BT1;
+        private System.Windows.Forms.Button BT2;
+        private System.Windows.Forms.Button BT3;
+        private System.Windows.Forms.Button BT4;
+        private System.Windows.Forms.Button BT5;
+        private System.Windows.Forms.Button BT6;
+        private System.Windows.Forms.Button BT7;
+        private System.Windows.Forms.Button BT8;
+        private System.Windows.Forms.Button BT9;
+        private System.Windows.Forms.Button BTPlus;
+        private System.Windows.Forms.Button BTMinus;
+        private System.Windows.Forms.Button BTadd;
+        private System.Windows.Forms.Button BTDivide;
+        private System.Windows.Forms.Button BTr;
+        private System.Windows.Forms.Button BT0;
+        private System.Windows.Forms.Button BTp;
+        private System.Windows.Forms.Button BTa;
+        private System.Windows.Forms.TextBox TEXTB1;
+    }
+}
+
